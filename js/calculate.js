1da5c7cca9fb076853646caf103ce883766e27bb
mirza-azam <mirzaazam884@gmail.com>
2023-11-07 12:44:43 +0600

    calculate saveing

--- js/calculate.js
+++ js/calculate.js
@@ -1,8 +1,4 @@
 
-// const foodField = document.getElementsByClassName('food-field');
-// const rentField = document.getElementsByClassName('rent-field');
-// const clothesField = document.getElementsByClassName('Clothes-field');
-
 function getValueFromInputField(value) {
     const inputField = document.getElementById(value);
     const inputFieldString = inputField.value;
@@ -10,12 +6,51 @@ function getValueFromInputField(value) {
     return inputFieldValue;
 }
 
-document.getElementById('calculate-btn').addEventListener('click', function () {
+
+function getTextFromInnerText(value) {
+    const textField = document.getElementById(value);
+    const textFieldString = textField.innerText;
+    const textFieldValue = parseFloat(textFieldString);
+    return textFieldValue;
+}
+
+
+function calculate() {
     const foodValue = getValueFromInputField('food-field');
     const rentValue = getValueFromInputField('rent-field');
     const clothesValue = getValueFromInputField('Clothes-field');
+    const incomeValue = getValueFromInputField('income-field');
+
     const totalExpenses = document.getElementById('total-expenses');
+    const balance = document.getElementById('balance');
 
     const expensesCalculate = foodValue + rentValue + clothesValue;
-    totalExpenses.innerText = expensesCalculate;
+    const totalExpensesBalance =  totalExpenses.innerText = expensesCalculate;
+    
+    const dueBalance = incomeValue - expensesCalculate;
+    const mainBalance = balance.innerText = dueBalance;
+    
+    return mainBalance, totalExpensesBalance;
+}
+
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    calculate();
+});
+
+document.getElementById('save-btn').addEventListener('click', function(){
+
+    const saveAmmount = document.getElementById('save-ammount');
+    const remainingBalance = document.getElementById('remaining-balance');
+
+    const mainBalanceValue = getTextFromInnerText('balance');
+    const totalExpenses = getTextFromInnerText('total-expenses');
+    
+    
+    const discountField = getValueFromInputField('discount-field');
+    const saveingAmmountValue = totalExpenses * discountField / 100;
+    saveAmmount.innerText = saveingAmmountValue;
+
+    const totalRemaining = mainBalanceValue + saveingAmmountValue;
+    remainingBalance.innerText = totalRemaining;
+    
 });
